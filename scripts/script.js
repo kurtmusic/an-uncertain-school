13d7992b8c1d377486ba562e6c311157dc21ec58
Kurt Ley <leymku.rt@gmail.com>
2020-07-09 07:33:23 +0800

    add random picture placer. fix z-index. add hover title

--- scripts/script.js
+++ scripts/script.js
@@ -16,6 +16,7 @@ openButton.addEventListener("click", function() {
   modal.classList.toggle("closed");
 });
 
+/* Fullscreen Function */
 /* Get the documentElement (<html>) to display the page in fullscreen */
 var elem = document.documentElement;
 
@@ -23,11 +24,11 @@ var elem = document.documentElement;
 function openFullscreen() {
   if (elem.requestFullscreen) {
     elem.requestFullscreen();
-  } else if (elem.mozRequestFullScreen) { /* Firefox */
+  } else if (elem.mozRequestFullScreen) { 
     elem.mozRequestFullScreen();
-  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+  } else if (elem.webkitRequestFullscreen) { 
     elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+  } else if (elem.msRequestFullscreen) { 
     elem.msRequestFullscreen();
   }
 }
@@ -36,11 +37,49 @@ function openFullscreen() {
 function closeFullscreen() {
   if (document.exitFullscreen) {
     document.exitFullscreen();
-  } else if (document.mozCancelFullScreen) { /* Firefox */
+  } else if (document.mozCancelFullScreen) { 
     document.mozCancelFullScreen();
-  } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+  } else if (document.webkitExitFullscreen) { 
     document.webkitExitFullscreen();
-  } else if (document.msExitFullscreen) { /* IE/Edge */
+  } else if (document.msExitFullscreen) { 
     document.msExitFullscreen();
   }
+}
+
+/* Fullscreen Fuynction End */
+
+
+let maxX, maxY;
+
+function placeImg() {
+    const imgSrcArray = ['../images/1.jpg','../images/2.jpg','../images/3.jpg','../images/4.jpg','../images/5.jpg','../images/6.png','../images/1.jpg','../images/2.jpg','../images/3.jpg','../images/4.jpg','../images/5.jpg','../images/6.png'];
+    const {random: r} = Math;
+    
+
+
+    
+    
+      
+      for(var i=0;i<imgSrcArray.length;i++){
+        var x = r() * maxX;
+        var y = r() * maxY;
+        var link = `<a class="img-1" href="#"><img class="images" style="left: ${x}px; top: ${y}px;" src="${imgSrcArray[i]}"/><span>This is image number ${i}</span></a>`;
+        var bodyHtml = document.getElementById("content-section").innerHTML;
+        document.getElementById("content-section").innerHTML = bodyHtml + link;
+      
+    }
+  
+
+}
+
+
+onload = function() {
+    maxX = 3000 - 128;
+    maxY = 3000 - 160;
+    placeImg();
+}
+
+onresize = function() {
+    maxX = 3000 - 128;
+    maxY = 3000 - 160;
 }
