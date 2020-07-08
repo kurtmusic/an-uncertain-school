456e838b11e0e18ea237b52080ff6b2246a9db83
Kurt Ley <leymku.rt@gmail.com>
2020-07-09 05:44:34 +0800

    Add scrollbar, fix popup module, add fullscreen capabilities

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,44 @@ feather.replace();
 $(function() {
   $("#dragMenu").draggable();
   });
+
+var modal = document.querySelector("#aboutPop");
+var closeButton = document.querySelector("#close");
+var openButton = document.querySelector("#aboutLink");
+
+closeButton.addEventListener("click", function() {
+  modal.classList.toggle("closed");
+});
+
+openButton.addEventListener("click", function() {
+  modal.classList.toggle("closed");
+});
+
+/* Get the documentElement (<html>) to display the page in fullscreen */
+var elem = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) { /* Firefox */
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE/Edge */
+    document.msExitFullscreen();
+  }
+}
